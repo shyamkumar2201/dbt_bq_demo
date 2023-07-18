6b4152cd479fb6ec2db29c297bd78a5e09758e6d
cloudetlbigdata@gmail.com <cloudetlbigdata@gmail.com>
2023-07-18 16:09:31 +0000

    updated the schema.yml file

--- models/R365/dimension/customers_revenue.sql
+++ models/R365/dimension/customers_revenue.sql
@@ -14,4 +14,4 @@ customers as (
 
 select cast(customer_id as string) as customer_id, number_of_orders as revenue from customers 
 union all
-select * from revenue
+select contract_id, contract_line_item_quantity from revenue
